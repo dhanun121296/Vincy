90dc0b62d7e98449a26af3eeb9ef446eaddf30f6
dhanun121296 <dhanun121296@gmail.com>
2022-05-13 12:41:28 +0530

    first commit

--- src/test/java/com/crm/orgtest/ContactTest.java
+++ src/test/java/com/crm/orgtest/ContactTest.java
@@ -7,9 +7,12 @@ public class ContactTest {
 	@Test
 	public void createContactTest()
 	{
+		//start
 		System.out.println("contact created");
 		String BROWSER = System.getProperty("browser");
 		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
 		
 	}
 }
