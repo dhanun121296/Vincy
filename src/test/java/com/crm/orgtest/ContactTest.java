a2f182e03101871dac2def91969f06d098ea7a6b
dhanun121296 <dhanun121296@gmail.com>
2022-05-13 12:39:26 +0530

    aaaa

---
+++ src/test/java/com/crm/orgtest/ContactTest.java
@@ -0,0 +1,15 @@
+package com.crm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("contact created");
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
+	}
+}
